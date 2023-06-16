2d720c473a632e62022c24fee48e284ba25d5a1c
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-15 20:31:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry print
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	printf("with proper grammer, but outcome in a piece of art,\n");
+	return (0);
+}
