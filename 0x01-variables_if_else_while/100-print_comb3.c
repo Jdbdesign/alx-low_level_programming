4d42ccfae06ee440cec1f27f278b121e3eb54a45
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-17 11:41:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,20 @@
  */
 int main(void)
 {
-    int n, m;
-    for (n = 0; n < 10; n++)
-    {
-        for (m = n + 1; m < 10; m++)
-        {
-            putchar(n + '0');
-            putchar(m + '0');
-
-            if (n != 8 || m != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	int n, m;
+	for (n = 0; n < 10; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
