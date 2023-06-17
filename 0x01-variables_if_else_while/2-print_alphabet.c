d9dd95612d4cccca57c7f6f5a257e107ae1dd4c2
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-16 17:21:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase[26] = "alphabet";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(lowercase[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
