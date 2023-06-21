5815522e25ca69c219b0ba41bf3373545421ccc0
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-20 23:42:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the 
+ * print_alphabet_x10 - prints 10 times the
  * alphabet, in lowercase,
  * followed by a new line
  */
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char ch;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
