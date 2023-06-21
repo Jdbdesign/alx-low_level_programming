955b1b33b64fd32006d2dcd3322b599565bd296f
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-21 00:01:37 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 
 /**
  * function that checks for lowercase character
- *
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 int _islower(int c)
