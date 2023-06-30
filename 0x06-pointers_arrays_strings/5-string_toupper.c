f604c120db2b9f7ac1f6018bd1a576191e4ddf25
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-30 14:07:21 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - function that changes all lowercase letters
- * of a string to uppercase. 
+ * of a string to uppercase.
  * @n: pointer
  *
  * Return: n
