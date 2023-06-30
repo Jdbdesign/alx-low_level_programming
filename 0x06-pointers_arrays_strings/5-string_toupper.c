3f5e9f587d8a9b78d8a5c26ff2254b01f7bb61e3
Jdbdesign <olayinkajacobs1964@gmail.com>
2023-06-30 01:39:58 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * function that changes all lowercase letters 
- * of a string to uppercase.
+ * string_toupper - function that changes all lowercase letters
+ * of a string to uppercase. 
  * @n: pointer
  *
  * Return: n
