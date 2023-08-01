abd56c95d3bdbc78370a33b0b0958ebad11f9e31
Jdbdesign <jacobsolayinka19@gmail.com>
2023-08-01 07:07:46 -0700

    Created a function that inserts a new node at a given position.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Insects a new node in a linked list at a
+ * given position
+ * @head: Pointer to the first node in the list.
+ * @idx: Index where the new node is added (starting at 0).
+ * @n: Data to insect in the new node.
+ *
+ * Return: Pointer to the new node, or NULL if it failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *temp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+	{
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = temp->next;
+	temp->next = new_node;
+
+	return (new_node);
+}
